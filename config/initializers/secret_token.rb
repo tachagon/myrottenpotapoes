c41789db20fa7ffeb12ef0bf0c9f47b7b0c4f226
saasbook <saasbook@saasbook.(none)>
2014-09-16 14:12:14 +0700

    Create new Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '5b4393c2f16466f2c09ccd7dbbe7185a531f014221a07f006f029ae2365f53f54e036da592c2c0214f6f99f0bd91d55b7fe17e8d29463a1725abec0b6042efa5'
