ef6d78d7adca5db1e8032fe6777b450f7c7ac5b6
saasbook <saasbook@saasbook.(none)>
2014-09-17 23:25:19 +0700

    For Chapter 4.7 Form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,14 @@ class MoviesController < ApplicationController
 		@movie = Movie.find_by_id(params[:id]) # what if this movie not in DB?
 #BUG: we should check @movie for validity here!
 	end
+	
+	def new
+		# default: render 'new' template
+	end
+
+	def create
+		@movie = Movie.create!(params[:movie])
+		flash[:notice] = "#{@movie.title} was successfully created."
+		redirect_to movies_path
+	end
 end
