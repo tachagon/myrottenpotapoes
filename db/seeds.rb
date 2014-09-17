71a671fa02ac043861e2beee92066b424d56d7ba
saasbook <saasbook@saasbook.(none)>
2014-09-17 12:30:07 +0700

    Edit seed.rb for add data in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+more_movies = [
+	{:title => 'Aladdin 2', :rating => 'G',
+		:release_date => '25-Nov-1992'},
+	{:title => 'When Harry Met Sally 2', :rating => 'R',
+		:release_date => '21-Jul-1989'},
+	{:title => 'The Help 2', :rating => 'PG-13',
+		:release_date => '10-Aug-2011'},
+	{:title => 'Raiders of the Lost Ark 2', :rating => 'PG',
+		:release_date => '12-Jun-1981'}
+	]
+
+more_movies.each do |movie|
+	Movie.create!(movie)
+end
