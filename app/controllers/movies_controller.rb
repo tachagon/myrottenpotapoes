c8bb15d6f289c0bf3a621743b49465f04034a71c
saasbook <saasbook@saasbook.(none)>
2014-09-18 08:10:21 +0700

    edit for CSS, add edit function, add update function and edit show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,4 +17,25 @@ class MoviesController < ApplicationController
 		flash[:notice] = "#{@movie.title} was successfully created."
 		redirect_to movies_path
 	end
+
+	def edit
+		@movie = Movie.find params[:id]
+	end
+
+	def update
+		@movie = Movie.find params[:id]
+		@movie.update_attributes!(params[:movie])
+
+		respond_to do |client_wants|
+			client_wants.html {redirect_to movie_path(@movie)}
+			client_wants.xml {render :xml => @movie.to_xml}
+		end
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		flash[:notice] = "Movie #{@movie.title} deleted."
+		redirect_to movies_path
+	end
 end
