983ed8bca0a7fe85ba49c97ab9ad683862102e19
saasbook <saasbook@saasbook.(none)>
2014-09-29 15:53:25 +0700

    make chapter5.1

--- app/controllers/application_controller.rb~
+++ app/controllers/application_controller.rb~
@@ -7,11 +7,4 @@ class ApplicationController < ActionController::Base
 		@current_user ||= Moviegoer.find_by_id(session[:user_id])
 		redirect_to login_path and return unless @current_user
 	end
-	
-	around_filter :only => ['withdraw_money', 'transfer_money'] do
-		start = Time.now
-		yield
-		logger.info params
-		logger.info (Time.now - start)
-	end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,13 +9,17 @@ class MoviesController < ApplicationController
 	end
 	
 	def new
-		# default: render 'new' template
+	    @movie = Movie.new
 	end
 
 	def create
-		@movie = Movie.create!(params[:movie])
-		flash[:notice] = "#{@movie.title} was successfully created."
-		redirect_to movies_path
+		@movie = Movie.new(params[:movie])
+		if @movie.save
+			flash[:notice] = "#{@movie.title} was successfully created."
+			redirect_to movies_path
+		else
+			render 'new' # note, 'new' template can access @movie's field values!
+		end
 	end
 
 	def edit
@@ -24,11 +28,11 @@ class MoviesController < ApplicationController
 
 	def update
 		@movie = Movie.find params[:id]
-		@movie.update_attributes!(params[:movie])
-
-		respond_to do |client_wants|
-			client_wants.html {redirect_to movie_path(@movie)}
-			client_wants.xml {render :xml => @movie.to_xml}
+		if @movie.update_attributes(params[:movie])
+			flash[:notice] = "#{@movie.title} was successfully updated."
+			redirect_to movie_path(@movie)
+		else
+		    render 'edit' # note, 'edit' template can access @movie's field values!
 		end
 	end
 
