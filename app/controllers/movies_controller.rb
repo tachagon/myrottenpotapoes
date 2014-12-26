56b69982c30e248df7327ac15fc90ade15e7633c
Tatchagon <o_k_t@hotmail.com>
2014-12-26 15:03:25 +0700

    make 8.3 Seams, Doubles, and the Code You Wish You Had

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -68,8 +68,6 @@ class MoviesController < ApplicationController
 	end
 	
 	def search_tmdb
-		# hardwire to simulate failure
-		flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
-		redirect_to movies_path
+		@movies = Movie.find_in_tmdb(params[:search_terms])
 	end
 end
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe MoviesController do
   describe 'searching TMDb' do
-    it 'should call the model method that performs TMDb search'
+    it 'should call the model method that performs TMDb search' do
+      fake_results = [mock('movie1'), mock('movie2')]
+      Movie.should_receive(:find_in_tmdb).with('hardware').
+        and_return(fake_results)
+      post :search_tmdb, {:search_terms => 'hardware'}
+    end
     it 'should select the Search Results template for rendering'
     it 'should make the TMDb search results available to that template'
   end
